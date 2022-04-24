e71fef216105b8fcf138504365b142053f0452f2
Ryspek Baibosunov <ryspek.baybosunov@gmail.com>
2022-04-24 21:38:11 +0600

    add POST, DELETE, PUT methods on both controllers

--- src/main/java/com/pluralsight/conferencedemo/controllers/SpeakersController.java
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SpeakersController.java
@@ -2,11 +2,9 @@ package com.pluralsight.conferencedemo.controllers;
 
 import com.pluralsight.conferencedemo.models.Speaker;
 import com.pluralsight.conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +23,27 @@ public class SpeakersController {
     @RequestMapping("{id}")
     public Speaker get(@PathVariable Long id) {
         return speakerRepository.getById(id);
+    }
+
+    @PostMapping
+    public Speaker create(@RequestBody final Speaker speaker) {
+        return speakerRepository.saveAndFlush(speaker);
+    }
+
+    @DeleteMapping
+    @RequestMapping("{id}")
+    public void delete(@PathVariable Long id) {
+        // Also need to check for children records before deleting.
+        speakerRepository.deleteById(id);
+    }
+
+    @PutMapping
+    @RequestMapping("{id}")
+    public Speaker update(@PathVariable Long id, @RequestBody final Speaker speaker) {
+        // because this is a PUT, we expect all attributes to be passed in. A PATCH would only need what ...
+        // TODO: Add validation that all attributes are passed in, otherwise return a 400 bad payload
+        Speaker existingSpeaker = speakerRepository.getById(id);
+        BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
+        return speakerRepository.saveAndFlush(existingSpeaker);
     }
 }
